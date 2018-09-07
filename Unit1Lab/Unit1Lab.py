3438986ed1ae920397f0fb459954b752ef59740c
Seantejones <20JonesWilliam@bprep.org>
2018-09-07 09:16:59 -0700

    Unit 1 Lab

---
+++ Unit1Lab/Unit1Lab.py
@@ -0,0 +1,17 @@
+print("I love Python")
+myName = input()
+print ("My name is "+ myName)
+print(2+2)
+print(7-8)
+print(8*3)
+print(2**2)
+print(4/2)
+print(15%2)
+num1 = input()
+print(num1)
+num2 = input()
+print(num2)
+print(num1 +num2)
+print(int(num1)+int(num2))
+
+
