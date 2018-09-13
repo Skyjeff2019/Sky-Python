2f22148285b4157793ba73e90b3ea9ac067cf9bb
Seantejones <20JonesWilliam@bprep.org>
2018-09-13 09:17:45 -0700

    Lab

---
+++ Unit2CLab/Unit2CLab.py
@@ -0,0 +1,15 @@
+numList = [ 0, 1, 2, 3 ,4 ,5 ,6 ,7, 8 ,9 ]
+print(numList)
+print(numList[0], numList[len(numList)-1]
+subList = numList[0:5]
+print(subList)
+subList.append(45)
+subList1=subList
+print(subList1)
+myClasses= [ "Math", "Science", "Python", "CMP", "Ignation formation", "English"]
+print(myClasses)
+myClasses.remove("Math")
+myClasses.remove("Science")
+print(myClasses)
+favClasses= myClasses.pop(3)
+print("My favorite class is Python")
