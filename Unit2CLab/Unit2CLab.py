4d12d978fbd56cbe832cfc916dda534df4a4eea6
Seantejones <20JonesWilliam@bprep.org>
2018-09-18 08:44:22 -0700

    Lab

--- Unit2CLab/Unit2CLab.py
+++ Unit2CLab/Unit2CLab.py
@@ -1,7 +1,9 @@
-numList = [ 0, 1, 2, 3 ,4 ,5 ,6 ,7, 8 ,9 ]
+numList = [0,1,2,3,4,5,6,7,8,9]
 print(numList)
-print(numList[0], numList[len(numList)-1]
+print(numList[0], numList[len(numList)-1])
+
 subList = numList[0:5]
+
 print(subList)
 subList.append(45)
 subList1=subList
