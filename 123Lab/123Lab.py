ad65530d93bd5913689ad56ec6e5d80cae45c84f
Seantejones <20JonesWilliam@bprep.org>
2018-09-24 09:18:42 -0700

    Lab123

---
+++ 123Lab/123Lab.py
@@ -0,0 +1,21 @@
+def main():
+    print ("Bellarmine student current grade point average")
+    grade1= int(input("What grade are you in currently?-"))
+    gradelist= [9,10,11,12]
+    return (grade1)
+
+main()
+
+if 9:
+    print("you are a freshmen")
+
+if 10:
+    print("you are a sophmore")
+
+if 11:
+    print("you are a junior")
+
+if 12:
+    print("you are a senior")
+
+
