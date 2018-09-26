09a42b23b2ad5efdd8f77187fef6bef7aa92ff1a
Seantejones <20JonesWilliam@bprep.org>
2018-09-26 10:08:02 -0700

    Lab123

--- 123Lab/123Lab.py
+++ 123Lab/123Lab.py
@@ -1,21 +1,64 @@
 def main():
+    print("start here")
     print ("Bellarmine student current grade point average")
-    grade1= int(input("What grade are you in currently?-"))
-    gradelist= [9,10,11,12]
-    return (grade1)
+    schoolgrade= int(input( "What grade are you in-"))
+    #call getYearInSchool with input as an argument
+    ## 1 argument gets passed to getYearInSchool
+    #getYearInSchool()
+    ##print long grade
 
-main()
+    ## create a list of 4 to 6 grades
+    ## cal calcAverageGrade function with list and number in list as arguments
+    ## 2 arguments get passed to calcAverageGrade
+    #getAverage()
+    ## print average grade
 
-if 9:
-    print("you are a freshmen")
+    ## call getLetterGrade funciton with average grade as argument
+    ## 1 argument gets passed to getLetterGrade
+    #getLetterGrade()
+    ## print letter grade
+    ## determine letter grade is passing or failing and print
 
-if 10:
-    print("you are a sophmore")
-
-if 11:
-    print("you are a junior")
-
-if 12:
-    print("you are a senior")
+    Mylist= [9,10,11,12]
+    Mylist2= [ 60.0,70.0,80.0,90.0]
 
 
+
+
+
+
+
+def getYearInSchool():
+    print("getYearInSchool")
+    ## get long grade and return long grade to main
+
+
+
+
+
+
+def getAverage():
+    print("getAverage")
+    ## calculate average grade and return value to main
+
+
+
+
+
+
+
+def getLetterGrade():
+    ## get letter grade and return letter grade to main
+    print("getLetterGrade")
+    if averagegrade >= 90
+        print("A")
+    if averagegrade == 80
+        print("B")
+    if averagegrade == 70
+        print("C")
+    if averagegrade ==60
+        print("F")
+
+
+
+#main()
