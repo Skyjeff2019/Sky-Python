6e985e9bef3c5799ab0ed8564210c31fa2ea02e4
Seantejones <20JonesWilliam@bprep.org>
2018-10-04 09:03:31 -0700

    Lab4A

--- Lab4A/Lab4A.py
+++ Lab4A/Lab4A.py
@@ -1,7 +1,22 @@
 def main():
-    Man = input( "print a string")
 
-def deVowel(Man):
-    novowel= ""
+    mystring = input('Input your string here - ')
 
+    print(deVowel(mystring))
 
+def deVowel(mystring):
+
+    noVowel = ''
+
+    for x in mystring:
+
+        if x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u':
+
+            y = 'p'
+
+        else:
+            noVowel = noVowel + x
+
+    return (noVowel)
+
+main()
