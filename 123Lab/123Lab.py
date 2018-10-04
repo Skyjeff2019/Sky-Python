3ceea28e8e066ba6697c2f4a5a5173178b98f79b
Seantejones <20JonesWilliam@bprep.org>
2018-10-04 08:48:59 -0700

    Lab123

--- 123Lab/123Lab.py
+++ 123Lab/123Lab.py
@@ -1,41 +1,52 @@
-def main():
-    print ("Bellarmine student current grade point average")
-    numList1= [ 93.4, 86.8, 98.2, 89.9, 94.6]
-    gradeIn= input("What year are you in school?- ")
-    grade1= int(gradeIn)
-    print("you are" + str(gradeIn))
-    print (" your current average score is-" + str(getAverage(numList1)))
-    print ("your current letter grade is-" + str(getLetterGrade(getAverage(numList1))))
+def main() :
 
-def getYearInSchool(gradeIn):
-    if gradeIn == 12:
-        return (str("a senior"))
-    elif gradeIn == 11:
-        return (str("a junior"))
-    elif gradeIn == 10:
-        return (str("a sophmore"))
-    elif gradeIn == 9:
-        return (str("a freshmen"))
+    gradeYear = input('What year are you in school - ')
+    strGrade = getgrade(gradeYear)
+    print (strGrade)
+    avgGrade = [92.5,95.6,96.6,98.9]
+    theAvggrade = avggpa(avgGrade,len(avgGrade))
+    strPercent = lettergrade(theAvggrade)
+    print(strPercent)
+    if strPercent == 'You have an A' or strPercent == 'You have a B' or strPercent == 'You have a C':
+        print('You are passing')
     else:
-        return (str(" you're not in highschool yet"))
-
-
-def getAverage():
+        print('You are failing')
 
 
 
+def getgrade(gradeYear):
+        if gradeYear == '9':
+            return('You are a freshman')
+        elif gradeYear == '10':
+            return('You are a sophmore')
+        elif gradeYear == '11':
+            return('You are a junior')
+        elif gradeYear == '12':
+            return('You are a senior')
+        else:
 
-def getLetterGrade(getAverage):
-    if getAverage >= 90:
-        lettergrade = (str("A"))
-    elif getAverage >= 80:
-        lettergrade = (str("B"))
-    elif getAverage >= 70:
-        lettergrade = (str("C"))
-    elif getAverage >= 60:
-        letterGrade = (str("D"))
-    else:
-        lettergrade = (str("F"))
-    return(letterGrade)
+            return('You are not in highschool')
+
+
+def avggpa(mylist,num) :
+
+        x = mylist[0] + mylist[1] + mylist[2] + mylist[3]
+        y = x / 4
+        return y
+
+def lettergrade(percentgrade):
+        if percentgrade >= 92.5:
+            return('You have an A')
+        elif percentgrade >= 88:
+            return('You have a B')
+        elif percentgrade >= 78:
+            return('You have a C')
+        elif percentgrade >= 72:
+            return ('You have a D')
+        elif percentgrade >= 65:
+            return ('You have a F')
+        else:
+            return('where were you all this time?')
+
 
 main()
--- Lab4A/Lab4A.py
+++ Lab4A/Lab4A.py
@@ -1,25 +1,7 @@
 def main():
-    myString = str(input( "print a string"))
-    deVowel()
+    Man = input( "print a string")
+
+def deVowel(Man):
+    novowel= ""
 
 
-
-
-
-def deVowel():
-    noVowels= ''
-    for vowels in myString:
-        if vowels == 'a':
-            noVowels= myString +vowels
-        if vowels == 'e':
-            noVowels= myString +vowels
-        if vowels == 'i':
-            noVowels= myString +vowels
-        if vowels == 'o':
-            noVowels == myString +vowels
-        if vowels =='u':
-            noVowels= myString +vowels
-        return noVowels
-
-
-main()
