da298a3c0bc11b174eae36cde82151334193d16e
Seantejones <20JonesWilliam@bprep.org>
2018-11-08 09:04:06 -0800

    Dicev2

--- Dice Projectv2/Dice Projectv2.py
+++ Dice Projectv2/Dice Projectv2.py
@@ -1,53 +1,112 @@
 import random
+
 ROLLNUM = 2
 
-def main():
-    setOfDice=[0] * 6 # create a list to store 6 different dice
-    setOfDice = defineDice()
-    printDiceSideBySide(setOfDice) # print all six dice
-    play = 'y'
-    game = 0
-# prime with y for yes and keep playing until n for no
-    while play == 'y':
-        for i in range(0, 22):
-            print('_', end ='') # print a line between each new roll
-    print()
-    game += 1
-    print(' game - ' + str(game))
-    play = input('play again - y or n - ') # keep playing or stop
-print('you played ' + str(game) + ' times')
-# defines 6 different dice counts and returns the characters
-# needed to print the dice to the console
+def dice():
+    loop = "y"
 
-def defineDice():
-    dice=[0] * 6
-    topBot  = ' -------- '
-    blank   = '|       |'
-    oneDotL = '| *     |'
-    oneDotM = '|   *   |'
-    oneDotR = '|     * |'
-    twoDot  = '|  * *  |'
-    for num in range(0, 6):
-        if num == 0:
-            dice[num] = [topBot,blank,oneDotM, blank,topBot]
-        elif num == 1:
-            dice[num] = [topBot,blank,twoDot,blank,topBot]
-        elif num == 2:
-            dice[num] = [topBot,oneDotL,oneDotM,oneDotR,topBot]
-        elif num == 3:
-            dice[num] = [topBot,twoDot,blank,twoDot,topBot]
-        elif num == 4:
-            dice[num] = [topBot,twoDot,oneDotM,twoDot,topBot]
-        else:
-            dice[num] = [topBot,twoDot,twoDot,twoDot,topBot]
-    return dice
-# generate a random number between 1 and 6 to represent random roll of dice
+    #setOfDice = createDice()
+    #printDiceSideBySide(setOfDice)
+    times = 0
+    while loop== "y":
+        diceList = [0]*ROLLNUM
+        #for i in range(ROLLNUM):
+        (roll, roll2) = randnum()
+        print(roll, roll2)
+        diceList = createDice(roll, roll2)
 
-def rollDice():
-    print('roll dice')
-# receives a list of the dice
+        printDiceSideBySide(diceList)
+            #printDiceSideBySide(diceSet)
+        times += 1
+
+        print ('you rolled this- ' + str(roll) + " and a " +str(roll2))
+
+        loop = input('would you like to play again (y/n)')
+
+        print('you have played ' + str(times) + " times")
 
 
-def printDiceSideBySide(diceSet):
-    print('print dice')
-main()
+
+def randnum():
+    x = random.randint(1,5)
+    y = random.randint(1,5)
+    return(x,y)
+
+def createDice(roll, roll2):
+   topBot = ' ------- '
+   blank1  = '|       |'
+   blankL  = '| *     |'
+   blankR  = '|     * |'
+   blankM  = '|   *   |'
+   blank2  = '|  * *  |'
+
+
+   onedice = [topBot, blank1, blankM, blank1, topBot]
+
+   twodice = [topBot, blank1, blank2, blank1, topBot]
+
+   threedice = [topBot, blankL, blankM, blankR, topBot]
+
+   fourdice = [topBot, blank2, blank1, blank2, topBot]
+
+   fivedice = [topBot, blank2, blankM, blank2, topBot]
+
+   sixdice = [topBot, blank2, blank2, blank2, topBot]
+
+   if roll == 1:
+       roll = onedice
+
+   elif roll == 2:
+       roll = twodice
+
+   elif roll == 3:
+       roll = threedice
+
+   elif roll == 4:
+       roll = fourdice
+
+   elif roll == 5:
+       roll = fivedice
+
+   elif roll == 6:
+       roll = sixdice
+
+   if roll2 == 1:
+       roll2 = onedice
+
+   elif roll2 == 2:
+       roll2 = twodice
+
+   elif roll2 == 3:
+       roll2 = threedice
+
+   elif roll2 == 4:
+       roll2 = fourdice
+
+   elif roll2 == 5:
+       roll2 = fivedice
+
+   elif roll2 == 6:
+       roll2 = sixdice
+
+   else:
+       print('error')
+
+   Dice1 = roll
+   Dice2 = roll2
+
+
+
+   allDice = [Dice1, Dice2]
+
+
+   return allDice
+
+def printDiceSideBySide(allDice):
+     for row in range(0,len(allDice[0])):
+         for col in range(0,len(allDice)):
+             print(allDice[col][row], end = '\t')
+         print()
+
+
+dice()
